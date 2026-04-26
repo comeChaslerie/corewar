a7dd213fcff25699b0d3f3a27f5c84ca0b250a71
comeChaslerie <come.chaslerie@epitech.eu>
2026-04-27 01:13:30 +0200

    <add>(game_loop.c):basic game_loop with dump calling and instructions cycle

---
+++ src/game_loop/game_loop.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2026
+** project
+** File description:
+** file
+*/
+
+#include <stdbool.h>
+#include "struct.h"
+#include "game.h"
+
+bool game_loop(main_t *main)
+{
+    if (!main)
+        return false;
+    for (unsigned int i = 0; i < CYCLE_TO_DIE; i++){
+        if (!apply_instructions(main))
+            return false;
+        if (main->args.cycle_dump == i)
+            dump(main);
+    }
+    return true;
+}
