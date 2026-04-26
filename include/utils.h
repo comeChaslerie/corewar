812e055031d588932d34bc74ffed4bbc7f6f7496
comeChaslerie <come.chaslerie@epitech.eu>
2026-04-27 01:14:25 +0200

    <fix>(utils.h):bool include added and strndup declaration added

--- include/utils.h
+++ include/utils.h
@@ -8,12 +8,15 @@
 #ifndef UTILS_H_
     #define UTILS_H_
 
+    #include <stdbool.h>
+
 void *put_error(const char *str, void *return_value);
 int my_strncmp(char *s1, char *s2, int end);
 int my_strcmp(char *s1, char *s2);
 int my_getnbr(char const *str, int start);
 int my_strlen(const char *str);
 char *my_strdup(char *src);
+char *my_strndup(char *src, int start, int end);
 bool is_nbr(char *str);
 
 #endif
