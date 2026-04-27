62a8e604b0d6106f7b71b1479af876efd34aa4fa
comeChaslerie <come.chaslerie@epitech.eu>
2026-04-27 22:36:06 +0200

    <add>(my_ustrndup.c):strndup with unsigned char * str function

---
+++ src/utils/my_ustrndup.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** my_strdup
+** File description:
+** duplicates a string
+*/
+#include <stdlib.h>
+
+unsigned char *my_ustrndup(unsigned char *src, unsigned int start,
+    unsigned int end)
+{
+    unsigned char *str;
+    unsigned int ulen = end - start;
+    int len = end - start;
+
+    if (!src || len < 0)
+        return NULL;
+    str = malloc(sizeof(char) * (ulen));
+    if (!str)
+        return NULL;
+    for (unsigned int i = 0; i < ulen; i++)
+        str[i] = src[start + i];
+    return (str);
+}
