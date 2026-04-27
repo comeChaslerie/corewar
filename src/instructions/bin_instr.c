0deea7a918c1f8d989c0552ac1d6eb5aef752369
comeChaslerie <come.chaslerie@epitech.eu>
2026-04-27 22:44:24 +0200

    <modify>(op.c && instructions/):instructions prototypes comforming with header declaration and op_tab declaration comforming

--- src/instructions/bin_instr.c
+++ src/instructions/bin_instr.c
@@ -5,18 +5,20 @@
 ** instructions functions bin calculous
 */
 #include <stdbool.h>
+#include "op_define.h"
+#include "struct.h"
 
-bool bin_and_instr(void *value)
+bool bin_and_instr(void *value, arg_t *args[MAX_ARGS_NUMBER])
 {
     return true;
 }
 
-bool bin_or_instr(void *value)
+bool bin_or_instr(void *value, arg_t *args[MAX_ARGS_NUMBER])
 {
     return true;
 }
 
-bool bin_xor_instr(void *value)
+bool bin_xor_instr(void *value, arg_t *args[MAX_ARGS_NUMBER])
 {
     return true;
 }
--- src/instructions/fork_instr.c
+++ src/instructions/fork_instr.c
@@ -5,13 +5,15 @@
 ** instructions functions load
 */
 #include <stdbool.h>
+#include "op_define.h"
+#include "struct.h"
 
-bool fork_long_instr(void *value)
+bool fork_long_instr(void *value, arg_t *args[MAX_ARGS_NUMBER])
 {
     return true;
 }
 
-bool fork_instr(void *value)
+bool fork_instr(void *value, arg_t *args[MAX_ARGS_NUMBER])
 {
     return true;
 }
