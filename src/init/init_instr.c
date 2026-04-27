a63703cc9fe7cd7d51d5445b58c6592feeffa136
comeChaslerie <come.chaslerie@epitech.eu>
2026-04-27 22:38:53 +0200

    <add>(init_arg.c && init_instr.c):arg and instr struct initialising functions

---
+++ src/init/init_instr.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2026
+** project
+** File description:
+** file
+*/
+
+#include <stdlib.h>
+#include "struct.h"
+#include "utils.h"
+
+instr_t *init_instr(void)
+{
+    instr_t *instr = malloc(sizeof(instr_t) * 1);
+
+    if (!instr)
+        return put_error("Error: malloc in init_instr.\n", NULL);
+    for (unsigned int i = 0; i < MAX_ARGS_NUMBER; i++)
+        instr->args[i] = NULL;
+    instr->id = 0;
+    return instr;
+}
