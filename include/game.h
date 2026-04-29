fc44e3336da918b4957f8382b910acc11409db25
comeChaslerie <come.chaslerie@epitech.eu>
2026-04-29 02:03:51 +0200

    <modify>(include/): adapt functions prototypes to new versions

--- include/game.h
+++ include/game.h
@@ -17,14 +17,12 @@ int main_functions(int argc, char **argv);
 
 bool apply_instructions(main_t *main);
 
-bool check_function(unsigned char *);
-
-unsigned int *get_args_size(unsigned char);
-
-unsigned int get_global_size(unsigned int *args_size);
-
 instr_t *translate_mem(unsigned char *instr);
 
 unsigned int get_size_from_id(unsigned int instr_id);
+
+unsigned int *get_coding_byte_tab(unsigned char elem);
+
+unsigned int get_global_size(unsigned int *tab, unsigned int *nbr_args);
 
 #endif
