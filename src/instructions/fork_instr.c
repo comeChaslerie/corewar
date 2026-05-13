22a79faf4c74d023662fc1bde3d658a1b84dab8b
Gabin Dreux-Templon <gabin.dreux-templon@epitech.eu>
2026-05-13 13:42:29 +0200

    <fix> (fork) : correcting new struct

--- src/instructions/fork_instr.c
+++ src/instructions/fork_instr.c
@@ -15,16 +15,15 @@ bool fork_long_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
     unsigned int robot_id)
 {
     main_t *main = (main_t *)value;
-    pos_infos_t *pos_infos = main->robots[robot_id].pos_infos;
-    pos_infos_t *child_pos = NULL;
     unsigned int offset = uctoui(args[0]->arg, T_IND);
+    unsigned int child_pos = 0;
 
     if (main->robots[robot_id].parent)
         return true;
     if (!cp_robot(&(main->robots[robot_id])))
         return false;
-    child_pos = main->robots[robot_id].child->pos_infos;
-    child_pos->pos_next_instr += offset;
+    child_pos = main->robots[robot_id].child->game_infos->pc;
+    main->robots[robot_id].child->game_infos->pc += offset;
     return true;
 }
 
@@ -39,7 +38,7 @@ bool fork_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
         return true;
     if (!cp_robot(&(main->robots[robot_id])))
         return false;
-    child_pos = main->robots[robot_id].child->pos_infos;
-    child_pos->pos_next_instr += offset % IDX_MOD;
+    child_pos = main->robots[robot_id].child->game_infos->pc;
+    main->robots[robot_id].child->game_infos->pc += offset % IDX_MOD;
     return true;
 }
