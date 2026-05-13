a8bf8645c2fc98c44635679d7ee48ab857363eb2
Gabin Dreux-Templon <gabin.dreux-templon@epitech.eu>
2026-05-13 13:25:17 +0200

    <add> (load instruction) : every load instruction

--- src/instructions/load_instr.c
+++ src/instructions/load_instr.c
@@ -17,7 +17,7 @@ bool load_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
     unsigned int reg_id = (unsigned int)args[1]->arg[0];
     robot_game_infos_t *infos = ((main_t *)value)->robots[robot_id].game_infos;
 
-    source += ((main_t *)value)->robots[robot_id].pos_infos->pos_next_instr;
+    source = (infos->pc + source % IDX_MOD) % MEM_SIZE;
     for (unsigned int i = 0; i < REG_SIZE; i++)
         infos->regs[reg_id][i] = ((main_t *)value)->arena[source + i];
     set_carry_null_reg(infos->regs[reg_id], infos);
@@ -27,17 +27,45 @@ bool load_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
 bool load_ind_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
     unsigned int robot_id)
 {
+    unsigned int vala = uctoui(args[0]->arg, args[0]->type);
+    unsigned int valb = uctoui(args[1]->arg, args[1]->type);
+    unsigned int reg_id = (unsigned int)args[2]->arg[0];
+    unsigned int source = (vala + valb);
+    robot_game_infos_t *infos = ((main_t *)value)->robots[robot_id].game_infos;
+
+    source = (infos->pc + source % IDX_MOD) % MEM_SIZE;
+    for (unsigned int i = 0; i < REG_SIZE; i++)
+        infos->regs[reg_id][i] = ((main_t *)value)->arena[source + i];
+    set_carry_null_reg(infos->regs[reg_id], infos);
     return true;
 }
 
 bool load_long_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
     unsigned int robot_id)
 {
+    unsigned int source = uctoui(args[0]->arg, args[0]->type);
+    unsigned int reg_id = (unsigned int)args[1]->arg[0];
+    robot_game_infos_t *infos = ((main_t *)value)->robots[robot_id].game_infos;
+
+    source = (infos->pc + source) % MEM_SIZE;
+    for (unsigned int i = 0; i < REG_SIZE; i++)
+        infos->regs[reg_id][i] = ((main_t *)value)->arena[source + i];
+    set_carry_null_reg(infos->regs[reg_id], infos);
     return true;
 }
 
 bool load_long_ind_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
     unsigned int robot_id)
 {
+    unsigned int vala = uctoui(args[0]->arg, args[0]->type);
+    unsigned int valb = uctoui(args[1]->arg, args[1]->type);
+    unsigned int reg_id = (unsigned int)args[2]->arg[0];
+    unsigned int source = (vala + valb);
+    robot_game_infos_t *infos = ((main_t *)value)->robots[robot_id].game_infos;
+
+    source = (infos->pc + source) % MEM_SIZE;
+    for (unsigned int i = 0; i < REG_SIZE; i++)
+        infos->regs[reg_id][i] = ((main_t *)value)->arena[source + i];
+    set_carry_null_reg(infos->regs[reg_id], infos);
     return true;
 }
