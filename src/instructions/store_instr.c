4023367e82fb4e98e05ebc8d106f2b7f297694d5
Gabin Dreux-Templon <gabin.dreux-templon@epitech.eu>
2026-05-13 23:14:33 +0200

    <fix> (ind_store) : change function for good args

--- src/instructions/store_instr.c
+++ src/instructions/store_instr.c
@@ -33,14 +33,16 @@ bool store_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
 bool store_ind_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
     unsigned int robot_id)
 {
-    unsigned int val_a = uctoui(args[0]->arg, args[0]->type);
-    unsigned int val_b = uctoui(args[1]->arg, args[1]->type);
-    unsigned int src = (unsigned int)args[2]->arg[0];
-    unsigned int dest = (val_a + val_b) % IDX_MOD;
+    unsigned int src = (unsigned int)args[0]->arg[0];
+    unsigned int val_a = uctoui(args[1]->arg, args[1]->type);
+    unsigned int val_b = uctoui(args[2]->arg, args[2]->type);
+    unsigned int dest = 0;
     main_t *main = (main_t *)value;
+    unsigned char **regs = main->robots[robot_id].game_infos->regs;
 
-    dest = (main->robots[robot_id].game_infos->pc + dest) % MEM_SIZE;
+    dest = (main->robots[robot_id].game_infos->pc + (val_a + val_b) % IDX_MOD)
+        % MEM_SIZE;
     for (unsigned int i = 0; i < REG_SIZE; i++)
-        main->arena[dest + i] = main->robots[robot_id].game_infos->regs[src][i];
+        main->arena[dest + i] = regs[src][i];
     return true;
 }
