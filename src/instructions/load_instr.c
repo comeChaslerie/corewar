98d922ad1ca8727e90ecc891a8c7de67837ab00c
comeChaslerie <come.chaslerie@epitech.eu>
2026-05-15 01:50:29 +0200

    <modify>(load_instr.c):fix arg conversion

--- src/instructions/load_instr.c
+++ src/instructions/load_instr.c
@@ -7,7 +7,6 @@
 #include <stdbool.h>
 #include "define.h"
 #include "struct.h"
-#include "compute.h"
 #include "utils.h"
 #include "game.h"
 #include <stdio.h>
@@ -16,7 +15,7 @@
 bool load_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
     unsigned int robot_id)
 {
-    unsigned int reg_id = uctoui(args[1]->arg, args[1]->size);
+    unsigned int reg_id = (unsigned int)args[1]->arg[0];
     robot_game_infos_t *infos = ((main_t *)value)->robots[robot_id].game_infos;
     unsigned char *to_load = get_arg_content(args[0], robot_id,
         (main_t *)value);
@@ -34,7 +33,7 @@ bool load_ind_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
     robot_game_infos_t *infos = ((main_t *)value)->robots[robot_id].game_infos;
     unsigned int arg_a = get_u_arg_content(args[0], robot_id, (main_t *)value);
     unsigned int arg_b = get_u_arg_content(args[1], robot_id, (main_t *)value);
-    unsigned int reg_id = uctoui(args[2]->arg, args[2]->size);
+    unsigned int reg_id = (unsigned int)args[2]->arg[0];
     unsigned int source = (infos->pc + (arg_a + arg_b) % IDX_MOD) % MEM_SIZE;
 
     for (unsigned int i = 0; i < REG_SIZE; i++)
@@ -49,7 +48,7 @@ bool load_long_instr(void *value, arg_t *args[MAX_ARGS_NUMBER],
 {
     robot_game_infos_t *infos = ((main_t *)value)->robots[robot_id].game_infos;
     unsigned int source = get_u_arg_content(args[0], robot_id, (main_t *)value);
-    unsigned int dest = uctoui(args[1]->arg, args[1]->size);
+    unsigned int dest = (unsigned int)args[1]->arg[0];
 
     source = (infos->pc + source) % MEM_SIZE;
     for (unsigned int i = 0; i < REG_SIZE; i++)
