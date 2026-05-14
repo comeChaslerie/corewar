7ac50f1ec3eab33093265938d10aafc7f418eba4
Gabin Dreux-Templon <gabin.dreux-templon@epitech.eu>
2026-05-14 15:49:51 +0200

    <add> (dump) : print child

--- include/define.h
+++ include/define.h
@@ -106,6 +106,7 @@ typedef char args_type_t;
     #define OPEN_PARENTHESE "("
     #define CLOSE_PARENTHESE ")"
     #define NB_SPACE 4
+    #define NB_SPACE_CHILD 5
     #define PC_SIZE 8
     #define NB_REG 16
     #define REGISTER_LETTER "r"
--- src/dump_flag.c
+++ src/dump_flag.c
@@ -95,10 +95,10 @@ static void print_robot_name(robot_infos_t *robots)
     write(1, NEW_LINE_CHAR, 1);
 }
 
-static void print_register(unsigned char **regs)
+static void print_register(unsigned char **regs, unsigned int nb_spaces)
 {
     for (unsigned int i = 0; i < NB_REG; i++) {
-        for (unsigned int j = 0; j < NB_SPACE; j++)
+        for (unsigned int j = 0; j < nb_spaces; j++)
             write(1, SPACE_CHAR, 1);
         write(1, REGISTER_LETTER, 1);
         my_put_nbr_u(i + 1);
@@ -113,13 +113,32 @@ static void print_register(unsigned char **regs)
     write(1, NEW_LINE_CHAR, 1);
 }
 
+static void print_child(robot_infos_t *child, unsigned int parent_id)
+{
+    if (child) {
+        write(1, SPACE_CHAR, 1);
+        print_robot_name(child);
+        print_register(child->game_infos->regs, NB_SPACE_CHILD);
+        for (unsigned int j = 0; j < NB_SPACE_CHILD; j++)
+            write(1, SPACE_CHAR, 1);
+        write(1, "PC : ", 5);
+        print_index_hexa(child->game_infos->pc, 1);
+        for (unsigned int j = 0; j < NB_SPACE_CHILD; j++)
+            write(1, SPACE_CHAR, 1);
+        write(1, "carry: ", 7);
+        my_puthexa(child->game_infos->carry);
+        write(1, NEW_LINE_CHAR, 1);
+        print_child(child->child, parent_id);
+    }
+}
+
 static void print_robot(main_t *main)
 {
     write(1, "Registers:", 10);
     write(1, NEW_LINE_CHAR, 1);
     for (unsigned int i = 0; i < main->nbr_robots; i++) {
         print_robot_name(&main->robots[i]);
-        print_register(main->robots[i].game_infos->regs);
+        print_register(main->robots[i].game_infos->regs, NB_SPACE);
         for (unsigned int j = 0; j < NB_SPACE; j++)
             write(1, SPACE_CHAR, 1);
         write(1, "PC : ", 5);
@@ -129,6 +148,7 @@ static void print_robot(main_t *main)
         write(1, "carry: ", 7);
         my_puthexa(main->robots[i].game_infos->carry);
         write(1, NEW_LINE_CHAR, 1);
+        print_child(main->robots[i].child, i);
     }
     write(1, NEW_LINE_CHAR, 1);
 }
