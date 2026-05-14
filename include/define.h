28399cf5b380119396d82d0d16156c6ec8d077f8
Gabin Dreux-Templon <gabin.dreux-templon@epitech.eu>
2026-05-14 18:07:13 +0200

    <modify> (dump) : modulable function

--- include/define.h
+++ include/define.h
@@ -111,4 +111,6 @@ typedef char args_type_t;
     #define NB_REG 16
     #define REGISTER_LETTER "r"
     #define NB_REG_LINE 6
+    #define NB_SPACE_NAME 2
+    #define NB_SPACE_NAME_CHILD 3
 #endif /* DEFINE_H_ */
