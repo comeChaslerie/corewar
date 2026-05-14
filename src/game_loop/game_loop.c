101484b503b934c6eb34835645673af3d8583eee
Stan Gaumain <stanislas.gaumain@epitech.eu>
2026-05-14 02:34:49 +0200

    <fix> (game_loop) : refactor the function to have less than 20 lines per functions

--- src/game_loop/game_loop.c
+++ src/game_loop/game_loop.c
@@ -19,20 +19,28 @@ static void kill_robots(main_t *main)
     }
 }
 
+static void display_winner(robot_infos_t *winner)
+{
+    write(1, "The player ", 11);
+    my_put_nbr_u(winner->id);
+    write(1, "(", 1);
+    write(1, winner->header.prog_name,
+        my_strlen(winner->header.prog_name));
+    write(1, ") has won.\n", 11);
+}
+
 static bool is_finish_game(main_t *main)
 {
     unsigned int nb_alive = 0;
-    unsigned int last_alive = 0;
+    robot_infos_t *winner = NULL;
 
     for (unsigned int i = 0; i < main->nbr_robots; i++)
         if (main->robots[i].game_infos->alive){
             nb_alive++;
-            last_alive = main->robots[i].id;
+            winner = &main->robots[i];
         }
     if (nb_alive == 1){
-        write(1, "Player: ", 9);
-        my_put_nbr_u(last_alive);
-        write(1, " has win!\n", 11);
+        display_winner(winner);
         return true;
     }
     if (!nb_alive){
@@ -42,11 +50,22 @@ static bool is_finish_game(main_t *main)
     return false;
 }
 
-static void reset_cycle(main_t *main, unsigned int *cycle,
-    unsigned int cycle_to_die)
+static bool run_one_cycle(main_t *main, unsigned int *i,
+    unsigned int *cycles_to_die)
 {
-    kill_robots(main);
-    *cycle %= cycle_to_die;
+    if (!apply_cycle(main))
+        return put_error("instr apply fail", false);
+    if (main->cycle_dump == main->total_cycles)
+        dump(main);
+    if (main->nb_live >= NBR_LIVE){
+        *cycles_to_die -= CYCLE_DELTA;
+        main->nb_live %= NBR_LIVE;
+    }
+    if (*i != 0 && *i >= *cycles_to_die) {
+        kill_robots(main);
+        *i %= *cycles_to_die;
+    }
+    return true;
 }
 
 bool game_loop(main_t *main)
@@ -56,16 +75,8 @@ bool game_loop(main_t *main)
     if (!main)
         return put_error("incorrect arg in game_loop\n", false);
     for (unsigned int i = 0; cycles_to_die > 0; i++){
-        if (!apply_cycle(main))
-            return put_error("instr apply fail", false);
-        if (main->cycle_dump == main->total_cycles)
-            dump(main);
-        if (main->nb_live >= NBR_LIVE){
-            cycles_to_die -= CYCLE_DELTA;
-            main->nb_live %= NBR_LIVE;
-        }
-        if (i != 0 && i >= cycles_to_die)
-            reset_cycle(main, &i, cycles_to_die);
+        if (!run_one_cycle(main, &i, &cycles_to_die))
+            return false;
         if (is_finish_game(main))
             return true;
         main->total_cycles++;
